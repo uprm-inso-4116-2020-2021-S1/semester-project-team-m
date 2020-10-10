0e885b4c99e9a2b2453dd1b3f45009680e6003c8
keviinramiirez <kevin.ramirez3@upr.edu>
2020-10-10 16:20:57 -0400

    basic angular app setup

---
+++ angular-app/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { AuthModule } from './auth/auth.module';
+import { MainModule } from './main/main.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AuthModule,
+    MainModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
