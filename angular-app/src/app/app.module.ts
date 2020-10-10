99a2345f68446a4feaa222f61b6e390c0b6c59a4
keviinramiirez <kevin.ramirez3@upr.edu>
2020-10-10 16:44:34 -0400

    added simple routing

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { Routes, RouterModule } from '@angular/router'
 
 import { AuthModule } from './auth/auth.module';
 import { MainModule } from './main/main.module';
+
+import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' }
+];
 
 @NgModule({
   declarations: [
@@ -15,6 +21,10 @@ import { MainModule } from './main/main.module';
     AppRoutingModule,
     AuthModule,
     MainModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular-app/src/app/main/main.module.ts
+++ angular-app/src/app/main/main.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main.component';
+import { Routes, RouterModule } from '@angular/router';
 
-
+const routes: Routes = [
+  { path: 'home', component: MainComponent }
+];
 
 @NgModule({
-  declarations: [MainComponent],
+  declarations: [
+    MainComponent,
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
+  ],
+  exports: [
+    RouterModule
   ]
 })
 export class MainModule { }
