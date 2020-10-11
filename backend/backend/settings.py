dd0f3616cec5a127359374228a04881b600e19a9
Gabriel J Rosa-Rios <gabriel.rosa4@upr.edu>
2020-10-11 17:53:23 -0400

    Allow db connection

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'atmosphere',
+        'USER': 'postgres',
+        'PASSWORD': 'd965444617a53b701426e4b705c5b082',
+        'HOST': 'terrain.gabrielrosa.dev',  #local
+        'PORT': '26366',    # local
     }
 }
 
