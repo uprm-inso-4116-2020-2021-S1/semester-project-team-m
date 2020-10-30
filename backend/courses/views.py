117d7548274b967f630728fec6123653aa7ca329
Gabriel J Rosa-Rios <gabriel.rosa4@upr.edu>
2020-10-29 23:44:36 -0400

    Add an example of how to create a dataframe

--- backend/courses/views.py
+++ backend/courses/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+import requests
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from .models import MyCourse
 
-# Create your views here.
+
+@login_required(login_url='/admin')
+def test_pandas(requests):
+    data = MyCourse.objects.all()
+    df = data.to_dataframe(fieldnames=['email', 'course__code', 'course__title', 'course__worth', 'grade', 'course__pre'])
+    return HttpResponse(df.to_html())
