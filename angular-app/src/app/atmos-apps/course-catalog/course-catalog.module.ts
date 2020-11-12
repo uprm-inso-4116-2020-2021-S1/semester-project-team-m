4af1b6dbe7302bc4851215676eedf0274c11e489
keviinramiirez <kevin.ramirez3@upr.edu>
2020-11-12 15:18:15 -0400

    removed unused component from this module

--- angular-app/src/app/atmos-apps/course-catalog/course-catalog.module.ts
+++ angular-app/src/app/atmos-apps/course-catalog/course-catalog.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
-import { CourseListComponent } from './course-list/course-list.component';
 import { CourseDetailsComponent } from './course-details/course-details.component';
-import { MatTableModule } from "@angular/material/table";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatInputModule } from "@angular/material/input";
-import { MatOptionModule } from "@angular/material/core";
-import { MatSelectModule } from "@angular/material/select";
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatOptionModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material';
 import { CourseCatalogContainerComponent } from './course-catalog-container/course-catalog-container.component';
 import { MaterialModule } from '../../material.module'
@@ -19,8 +18,6 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [
-    // MainComponent,
-    CourseListComponent,
     CourseDetailsComponent,
     CourseCatalogContainerComponent
   ],
@@ -38,7 +35,6 @@ const routes: Routes = [
   ],
   exports: [
     RouterModule,
-    CourseListComponent,
     CourseDetailsComponent,
     CourseCatalogContainerComponent
   ],
