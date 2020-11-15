768b4ebe39335e1db31c654b2826506bf5580e14
keviinramiirez <kevin.ramirez3@upr.edu>
2020-11-15 19:24:00 -0400

    changed name of project to atmosphere-app

---
+++ atmosphere-app/src/app/app.component.ts
@@ -0,0 +1,49 @@
+import { AfterViewInit, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Subscription } from 'rxjs';
+import { MediaChange, MediaObserver } from '@angular/flex-layout';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit, AfterViewInit {
+  title = 'atmosphere-app';
+
+  private mediaSub: Subscription;
+
+  constructor(
+    private cdRef: ChangeDetectorRef,
+    private mediaObserver: MediaObserver
+  ) { }
+
+  ngOnInit() {
+    this.mediaSub = this.mediaObserver.media$.subscribe(
+      (change: MediaChange) => {
+        console.log(change)
+        // console.log(change.mqAlial)
+      }
+    )
+  }
+
+
+  ngAfterViewInit() {
+
+  }
+
+  ngOnDestroy() {
+
+  }
+
+  // url: string = 'http://127.0.0.1:8000/api/courses/';
+  //
+  // constructor(private http: HttpClient) {
+  // }
+  //
+  // public getLocations() {
+  //   this.http.get(this.url).toPromise().then((data) => {
+  //     console.log(data);
+  //   });
+  // }
+}
