af82a632b0279109ea154299d22e1c1fb938e323
keviinramiirez <kevin.ramirez3@upr.edu>
2020-11-21 16:25:03 -0400

    used the Course model with  fetched courses

--- atmosphere-app/src/app/business-logic/course-catalog/course-catalog.service.ts
+++ atmosphere-app/src/app/business-logic/course-catalog/course-catalog.service.ts
@@ -1,6 +1,9 @@
+import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { AuthenticationService } from '../authentication/authentication.service';
+import { Course } from '../models/course';
+import { map } from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +14,8 @@ export class CourseCatalogService {
     'Content-Type': 'application/json',
   })
 
+
+
   constructor(
     private http: HttpClient,
     private authService: AuthenticationService
@@ -18,13 +23,50 @@ export class CourseCatalogService {
 
   }
 
-
-  getCourses() {
-    return this.http.get(`${this.baseUrl}api/courses/`, {
+  getAllCourses(): Observable<Course[]> {
+    return this.http.get<Course[]>(`${this.baseUrl}api/courses/`, {
       headers: this.authService.getAuthHeaders(),
       responseType: 'json'
-    })
+    }).pipe(map(courses => {
+      return courses.map(data => {
+        return new Course(
+          data['code'],
+          data['title'],
+          data['worth'],
+          data['pre'],
+          data['curriculum']
+        )
+      })
+    }))
   }
 
+  getMyCourses(): Observable<Course[]> {
+    return this.http.get<Course[]>(`${this.baseUrl}api/mycourses/`, {
+      headers: this.authService.getAuthHeaders(),
+      responseType: 'json'
+    }).pipe(map(mycourses => {
+      return mycourses.map(myData => {
+        const c = myData['course'];
+        // const fieldGrade = 69 - field.value.charCodeAt(0);
 
+        // const grade = Number(69 - (myData['grade'] as string).charCodeAt(0));
+        // console.log("grade")
+        // console.log(grade)
+
+        return new Course(
+          c['code'],
+          c['title'],
+          c['worth'],
+          c['pre'],
+          c['curriculum'],
+          myData['grade'],
+          myData['term']
+        )
+      })
+    }))
+  }
+
+  getCurriculum() {
+
+  }
 }
--- atmosphere-app/src/app/business-logic/models/course.ts
+++ atmosphere-app/src/app/business-logic/models/course.ts
@@ -3,15 +3,20 @@ export interface Course {
   title: string;
   worth: number;
   pre: string[];
+  curriculum: string[];
   grade?: string;
 }
 
-// export class Course {
-//   constructor(
-//     code: string,
-//     title: string,
-//     worth: number,
-//     pre: string[],
-//     grade?: string,
-//   ) { }
-// }
+
+
+export class Course {
+  constructor(
+    public code: string,
+    public title: string,
+    public worth: number,
+    public pre: string[],
+    public curriculum: string[],
+    public grade?: string, //{A, B, C, D, F} = {4, 3, 2, 1, 0} respectively
+    public term?: string,
+  ) { }
+}
