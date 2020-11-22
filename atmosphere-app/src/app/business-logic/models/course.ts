2e5621c5b60fc9ced5a8445afee30e4d0db9f891
keviinramiirez <kevin.ramirez3@upr.edu>
2020-11-22 16:47:03 -0400

    curriculum optional

--- atmosphere-app/src/app/business-logic/models/course.ts
+++ atmosphere-app/src/app/business-logic/models/course.ts
@@ -3,7 +3,7 @@ export interface Course {
   title: string;
   worth: number;
   pre: string[];
-  curriculum: string[];
+  curriculum?: string[];
   grade?: string;
 }
 
@@ -15,7 +15,7 @@ export class Course {
     public title: string,
     public worth: number,
     public pre: string[],
-    public curriculum: string[],
+    public curriculum?: string[],
     public grade?: string, //{A, B, C, D, F} = {4, 3, 2, 1, 0} respectively
     public term?: string,
   ) { }
