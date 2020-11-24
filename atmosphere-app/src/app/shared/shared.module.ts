22941153e14270a4e6465d72d58976633a840306
keviinramiirez <kevin.ramirez3@upr.edu>
2020-11-24 11:13:04 -0400

    removed sidebar

--- atmosphere-app/src/app/shared/shared.module.ts
+++ atmosphere-app/src/app/shared/shared.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SidebarComponent } from './comnponents/sidebar/sidebar.component';
 import { HeaderComponent } from './comnponents/header/header.component';
 import { FooterComponent } from './comnponents/footer/footer.component';
 import { RouterModule } from '@angular/router';
@@ -9,22 +8,21 @@ import { MatButtonModule } from '@angular/material';
 import { MaterialModule } from '../material.module';
 
 
+
 @NgModule({
   declarations: [
     HeaderComponent,
-    SidebarComponent,
     FooterComponent,
   ],
   imports: [
     CommonModule,
     RouterModule,
     FlexLayoutModule,
-    MaterialModule
+    MaterialModule,
   ],
   exports: [
     HeaderComponent,
-    SidebarComponent,
-    FooterComponent
+    FooterComponent,
   ]
 })
 export class SharedModule { }
