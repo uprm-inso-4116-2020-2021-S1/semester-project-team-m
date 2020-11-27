00c4bc75ee112eb5ca21f995c737947ebd324ece
keviinramiirez <kevin.ramirez3@upr.edu>
2020-11-27 13:30:05 -0400

    more robust logic and fields

--- atmosphere-app/src/app/terrain-apps/course-catalog/course-create/course-create.component.ts
+++ atmosphere-app/src/app/terrain-apps/course-catalog/course-create/course-create.component.ts
@@ -16,9 +16,11 @@ export class CourseCreateComponent implements OnInit {
   // public courseForm: FormGroup;
   public codeOptions: string[] = [];
   public codeForm = new FormControl('Choose Code');
+  public gradeOptions: string[] = [];
+  public gradeForm = new FormControl('Choose Grade');
   // public code: string;
-  public grade: string;
-  public term: string;
+  // public grade: string;
+  public term: number;
 
   constructor(
     private courseCatalogService: CourseCatalogService,
@@ -27,6 +29,12 @@ export class CourseCreateComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    this.gradeOptions.push('A')
+    this.gradeOptions.push('B')
+    this.gradeOptions.push('C')
+    this.gradeOptions.push('D')
+    this.gradeOptions.push('F')
+
     this.courseCatalogService.getCurriculum().subscribe(courses => {
       courses.forEach(c => {
         this.codeOptions.push(c.code)
@@ -35,31 +43,36 @@ export class CourseCreateComponent implements OnInit {
   }
 
   addToMycourse() {
-    if (!this.grade)
+    console.log(!this.term, (this.term < 1980 && this.term > 2020))
+    if (!this.codeForm.value || this.codeForm.value === 'Choose Code')
+      this.toast.infoToast('Please provide the code');
+    else if (!this.gradeForm.value || this.gradeForm.value.length > 1)
       this.toast.infoToast('Please provide the grade');
     else if (!this.term)
       this.toast.infoToast('Please provide the term (year)');
+    else if (!this.term || (this.term < 1980 || this.term > 2020))
+      this.toast.infoToast('Please provide a valid term')
     else {
       console.log(this.codeForm.value)
       //harcode for testing
-      const newCourse = new Course(
-        "INGE3011",
-        "",
-        0,
-        [],
-        [],
-        "B",
-        "2020",
-      );
       // const newCourse = new Course(
-      //   this.code,
+      //   "INGE3011",
       //   "",
       //   0,
       //   [],
       //   [],
-      //   this.grade,
-      //   this.term,
-      // )
+      //   "A",
+      //   "2020",
+      // );
+      const newCourse = new Course(
+        this.codeForm.value,
+        "",
+        0,
+        [],
+        [],
+        this.gradeForm.value,
+        String(this.term),
+      )
 
       this.courseCatalogService.getMycourseByCode(newCourse.code).subscribe(c => {
         this.courseCatalogService.postCourseToMycourse(newCourse).subscribe(_ => {
@@ -74,5 +87,11 @@ export class CourseCreateComponent implements OnInit {
 
   delay(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+
+  /** Returns true if given string is numeric. */
+  isNumeric(value: string): boolean {
+    return value != null && !isNaN(Number(value));
   }
 }
